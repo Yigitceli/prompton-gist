79ad2acd00472ca014bd1bd18c04459abd111127
Yigitceli <yigit.celi@outlook.com>
2024-01-28 21:03:55 +0300

    test init

---
+++ __mocks__/axios.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosResponse } from 'axios';
+
+// Axios'u doğrudan mockla
+const mockAxios = axios as jest.Mocked<typeof axios>;
+
+// Axios post metodunu güncelle ve doğru tiplemeleri sağla
+mockAxios.post.mockResolvedValueOnce({
+  data: {
+    choices: [{ message: 'Assistant 1 response' }],
+    usage: { total_tokens: 42 },
+  },
+} as AxiosResponse);
+
+mockAxios.post.mockResolvedValueOnce({
+  data: {
+    choices: [{ message: 'Assistant 2 response' }],
+    usage: { total_tokens: 24 },
+  },
+} as AxiosResponse);
+
+export default mockAxios;
